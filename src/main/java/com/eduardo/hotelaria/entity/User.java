54a437dfc266239ca0b6410200ded571d3c073b5
EduardoDandolini <eduardodasilvadandolini@hmail.com>
2024-01-04 19:38:04 -0300

    Initial commit

---
+++ src/main/java/com/eduardo/hotelaria/entity/User.java
@@ -0,0 +1,17 @@
+package com.eduardo.hotelaria.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    
+}
